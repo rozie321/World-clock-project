2dcb3539cb27288f36b5be0e060ef6dce62593a7
rozie321 <107440478+rozie321@users.noreply.github.com>
2024-06-22 22:29:38 +0100

    index.js updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,26 @@
-let losangelesElement=document.querySelector("los-angeles");
-let losangelesTimeelement=losangelesElement.querySelector(".date");
-let losangelesDateelement=losangelesElement.querySelector(".time");
-let losengelesTime=moment()
+function updateTime(){
 
 
-losangelesTimeelement.innerHTML=losengelesTime.format("MMMM Do YYY")
-losangelesDateelement.innerHTML=`{losengelesDate.format("h:mm")} <small> AM <small>`
+//sydney
+let sydneyElement=document.querySelector("#sydney");
+let sydneyDateElement=sydneyElement.querySelector(".date");
+let sydneyTimeElement=sydneyElement.querySelector(".time");
+let sydneyTime=moment().tz("Australia/Sydney");
+
+sydneyDateElement.innerHTML=sydneyTime.format("MMMM Do YYYY")
+sydneyTimeElement.innerHTML=`${sydneyTime.format("h:mm:ss")}<small>${sydneyTime.format("A")}</small>`
+
+
+//paris
+
+let parisElement=document.querySelector("#paris");
+let parisDateElement=parisElement.querySelector(".date");
+let parisTimeElement=parisElement.querySelector(".time");
+let parisTime=moment().tz("Europe/Paris");
+
+parisDateElement.innerHTML=parisTime.format("MMMM Do YYYY")
+parisTimeElement.innerHTML=`${parisTime.format("h:mm:ss")}<small>${parisTime.format("A")}</small>`
+
+}
+updateTime();
+setInterval(updateTime, 1000);
